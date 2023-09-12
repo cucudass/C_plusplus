2125ae0c226a137f4a2fd7a7e0e2a634bd72e6e3
조광래 <whrhkdfo12@naver.com>
2023-09-12 19:47:36 +0900

    230912_얕은 복사_깊은 복사

--- Solution/Solution/Solution.cpp
+++ Solution/Solution/Solution.cpp
@@ -1,77 +1,68 @@
 ﻿#include <iostream>
 using namespace std;
 
-#pragma region 클래스
-//사용자 정의 데이터 유형으로 속성과 함수가 포함되어 있으며, 클래스를 통해 객체를 생성하여 접근하고 사용할 수 있는 집합체
-
-class GameObject {
-	//접근지정자
-	// 클래스 내부의 포함되어 있는 속성에 접근 범위를 제한하는 지정자.
-	//public -> 클래스 내부와 자기가 상속하고 있는 클래스 그리고 클래스 외부에서 접근을 허용하는 지정자
-	//protected -> 클래스 내부와 자기가 상속하고 있는 클래스까지만, 접근을 허용하는 지정자
-	//private -> 클래스 내부에서만 접근을 허용하는 지정자
-
-	//기본 접근 지정자(private)	
+#pragma region 복사 생성자
+	//같은 객체를 복사하여 생성시킬 때 호출되는 생성자	
+class Item {
+	//기본 생성자
+	//public: item() { }
+private:
+	int price;
+	int *size = new int;
 
 public:
-	float x;
-	void SendMessage() {
-		cout << "메시지" << endl;
+	Item() {
+		price = 100;
+		*size = 100;
 	}
-protected:
-	float y;
-private:
-	double z;
+
+	Item(const Item &item) {
+		price = item.price;
+		size = new int;
+		*size = 9;
+	}
+
+	void Show() {
+		cout << "price: " << price << endl;
+		cout << "*size: " << *size << endl;
+	}
+
+	~Item() {
+		delete size;
+	}
 };
-#pragma endregion 
+#pragma endregion
 
-#pragma region 생성자
-	//클래스의 인스턴스가 생성되는 시점에서 자동으로 호출되는 특수한 멤버 함수
-class Monster {
-private :
-	int health;
-
-public :
-	Monster() {
-		cout << "Monster 생성" << endl;
-	}
-
+#pragma region 얕은 복사
+	//객체를 복사할 때 주소 값을 복사하여 같은 메모리를 가리키는 복사
+	//int *ptr1 = new int;
+	//int *ptr2 = ptr1;
+	//
+	//*ptr2 = 1000;
+	//
+	//cout << "ptr1의 가리키는 값: " << *ptr1 << endl;
+	//cout << "ptr2의 가리키는 값: " << *ptr2 << endl;
+	//
+	//delete ptr1;
+	// delete ptr2; ERROR
 	/*
-	생성자의 경우 객체가 생성될 때 단 한 번만 호출되며, 생성자는 반환형이 존재하지 않기 때문에 
-	생성자가 호출되기 전에는 객체에 대한 메모리는 할당하지 않는다.
+	얕은 복사는 메모리에서 해제될 때 생성자의 실행 순서와 반대로 소멸자가 실행됩니다.
+	복사한 객체가 먼저 해제됨으로, 원래 있던 객체가 해제될 때는 이미 해제된 메모리에 접근하게 된다.
 	*/
+#pragma endregion
 
-	//소멸자 객체가 소멸될때 자도으로 실행되는 클래스의 멤버 함수
-	~Monster() {
-		cout << "Monster 소멸" << endl;
-	}
-	/*
-	소멸자는 객체가 메모리에서 해제될 때 단 한 번만 호출되며,
-	소멸자에는 매개변수를 생성하여 사용할 수 없다.
-	*/
-};
+#pragma region 깊은 복사
+	//객체를 복사할 때, 참조 값이 아닌 인스턴스 자체를 새로 복사하여 서로 다른 메모리를 생성하는 복사
+
+
 #pragma endregion
 
 
 int main() {
-
-#pragma region 클래스
-	//GameObject gameobject1;
-	//GameObject gameobject2;
-	//
-	//cout << sizeof(gameobject1) << endl;
-	//gameobject1.SendMessage();
-	//gameobject2.SendMessage();
-	//
-	//gameobject1.x = 100;
-#pragma endregion
-
-#pragma region 생성자, 소멸자
-	Monster monster;
-	Monster *ptr = new Monster;
-	delete ptr;
-#pragma endregion
-
+	Item item1;
+	Item item2 = item1;
+	item1.Show();
+	item2.Show();
 
 	return 0;
 }
