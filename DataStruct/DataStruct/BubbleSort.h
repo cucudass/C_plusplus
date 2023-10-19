d208094eeb09bc9b66d75ea80d930185533eaefc
조광래 <whrhkdfo12@naver.com>
2023-10-19 19:47:47 +0900

    231019 선택정렬

---
+++ DataStruct/DataStruct/BubbleSort.h
@@ -0,0 +1,105 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < (n - i) - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
+#pragma region 연산자 오버로딩
+class Vector2 {
+private:
+	int x;
+	int y;
+public:
+	Vector2(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	Vector2 operator+(const Vector2 & vector2) {
+		Vector2 clone(x + vector2.x, y + vector2.y);
+
+		return clone;
+	}
+	Vector2 operator-(const Vector2 & vector2) {
+		Vector2 clone(x - vector2.x, y - vector2.y);
+
+		return clone;
+	}
+	Vector2 operator*(const Vector2 & vector2) {
+		Vector2 clone(x * vector2.x, y * vector2.y);
+
+		return clone;
+	}
+	Vector2 operator/(const Vector2 & vector2) {
+		Vector2 clone(x / vector2.x, y / vector2.y);
+
+		return clone;
+	}
+	Vector2 operator++() {
+		this->x++;
+		this->y++;
+
+		return *this;
+	}
+	Vector2 operator++(int) {
+		Vector2 clone = *this;
+		this->x++;
+		this->y++;
+
+		return clone;
+	}
+	int GetX() {
+		return x;
+	}
+	int GetY() {
+		return y;
+	}
+};
+#pragma endregion
+
+int main()
+{
+#pragma region 거품 정렬
+	/*
+	서로 인접한 두 원소를 검사하여 정렬하는 알고리즘
+	시간 복잡도 O(n^2)
+	*/
+	int arr[] = { 4,5,6,8,2 };
+	int n = sizeof(arr) / sizeof(int);
+
+	bubbleSort(arr, n);
+
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+#pragma endregion
+
+#pragma region 연산자 오버로딩
+	Vector2 Up(0, 1);
+	Vector2 Right(1, 0);
+
+	Vector2 temp = Up + Right;
+	cout << temp.GetX() << endl;
+	cout << temp.GetY() << endl;
+
+	Vector2 a = temp++;
+	cout << a.GetX() << endl;
+	cout << a.GetY() << endl;
+
+	++temp;
+	cout << temp.GetX() << endl;
+	cout << temp.GetY() << endl;
+
+#pragma endregion
+}
+
+
--- DataStruct/DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct/DataStruct.cpp
@@ -1,90 +1,38 @@
 ﻿#include <iostream>
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
-	for (int i = 0; i < n - 1; i++) {
-		for (int j = 0; j < (n - i) - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				swap(arr[j], arr[j + 1]);
+void Selectsort(int arr[], int n) {
+	for (int i = 0; i < n; i++) {
+		int min = arr[i];
+		int select = i;
+		for (int j = (i + 1); j < n; j++) {
+			if (min > arr[j]) {
+				min = arr[j];
+				select = j;
 			}
 		}
+		swap(arr[i], arr[select]);
 	}
 }
 
-#pragma region 연산자 오버로딩
-class Vector2 {
-private:
-	int x;
-	int y;
-public:
-	Vector2(int x, int y) {
-		this->x = x;
-		this->y = y;
-	}
-
-	Vector2 operator+(const Vector2 & vector2) {
-		Vector2 clone(x + vector2.x, y + vector2.y);
-		
-		return clone;
-	}
-
-	Vector2 operator-(const Vector2 & vector2) {
-		Vector2 clone(x - vector2.x, y - vector2.y);
-
-		return clone;
-	}
-
-	Vector2 operator*(const Vector2 & vector2) {
-		Vector2 clone(x * vector2.x, y * vector2.y);
-
-		return clone;
-	}
-
-	Vector2 operator/(const Vector2 & vector2) {
-		Vector2 clone(x / vector2.x, y / vector2.y);
-
-		return clone;
-	}
-
-	int GetX() {
-		return x;
-	}
-	int GetY() {
-		return y;
-	}
-};
-#pragma endregion
-
-
 int main()
 {
-#pragma region 거품 정렬
+#pragma region 선택정렬
 	/*
-	서로 인접한 두 원소를 검사하여 정렬하는 알고리즘
-	시간 복잡도 O(n^2)
+	정렬되지 않은 데이터들에 대해 가장 작은 데이터를 찾아서 가장 앞에 있는 데이터와 교환하는 알고리즘
 	*/
-	int arr[] = { 4,5,6,8,2 };
+
+	int arr[] = { 6,2,8,1,0 };
 	int n = sizeof(arr) / sizeof(int);
 
-	bubbleSort(arr, n);
+	Selectsort(arr, n);
 
-	for (int i = 0; i < 5; i++) {
-		cout << arr[i] << " ";
+	for (int & element : arr) {
+		cout << element << " ";
 	}
 	cout << endl;
 #pragma endregion
 
-#pragma region 연산자 오버로딩
-	Vector2 Up(0, 1);
-	Vector2 Right(1, 0);
-
-	Vector2 temp = Up + Right;
-
-	cout << temp.GetX() << endl;
-	cout << temp.GetY() << endl;
-#pragma endregion
-
-	
 }
 
 
