91415e5156d2e7ae6ee30311f5e7a1c84406a8cf
조광래 <whrhkdfo12@naver.com>
2023-10-19 15:25:18 +0900

    231018_버블정렬

--- DataStruct/DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct/DataStruct.cpp
@@ -1,30 +1,90 @@
-﻿#include "SingleLinkedList.h"
-#include "DoubleLinkedList.h"
-#include "CircularLinkedList.h"
-#include <string>
+﻿#include <iostream>
 using namespace std;
+
+void bubbleSort(int arr[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < (n - i) - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
+#pragma region 연산자 오버로딩
+class Vector2 {
+private:
+	int x;
+	int y;
+public:
+	Vector2(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+
+	Vector2 operator+(const Vector2 & vector2) {
+		Vector2 clone(x + vector2.x, y + vector2.y);
+		
+		return clone;
+	}
+
+	Vector2 operator-(const Vector2 & vector2) {
+		Vector2 clone(x - vector2.x, y - vector2.y);
+
+		return clone;
+	}
+
+	Vector2 operator*(const Vector2 & vector2) {
+		Vector2 clone(x * vector2.x, y * vector2.y);
+
+		return clone;
+	}
+
+	Vector2 operator/(const Vector2 & vector2) {
+		Vector2 clone(x / vector2.x, y / vector2.y);
+
+		return clone;
+	}
+
+	int GetX() {
+		return x;
+	}
+	int GetY() {
+		return y;
+	}
+};
+#pragma endregion
+
 
 int main()
 {
-	CircularLinkedList<int> clist;
+#pragma region 거품 정렬
+	/*
+	서로 인접한 두 원소를 검사하여 정렬하는 알고리즘
+	시간 복잡도 O(n^2)
+	*/
+	int arr[] = { 4,5,6,8,2 };
+	int n = sizeof(arr) / sizeof(int);
 
-	clist.PushBack(10);
-	clist.PushBack(20);
-	clist.PushBack(30);
-	clist.PushBack(40);
+	bubbleSort(arr, n);
 
-	clist.PushFront(5);
-	clist.PushFront(4);
-	clist.PushFront(3);
-	clist.Display();
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+#pragma endregion
+
+#pragma region 연산자 오버로딩
+	Vector2 Up(0, 1);
+	Vector2 Right(1, 0);
+
+	Vector2 temp = Up + Right;
+
+	cout << temp.GetX() << endl;
+	cout << temp.GetY() << endl;
+#pragma endregion
+
 	
-	cout << "\n";
-	cout << "PopBack\n";
-	clist.PopBack();
-	clist.Display();
-
-	cout << "\n";
-	cout << "PopFront\n";
-	clist.PopFront();
-	clist.Display();
 }
+
+
