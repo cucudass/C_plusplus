12acbc8916b8eb7ffd1f735c7e5a86d295542a4e
조광래 <whrhkdfo12@naver.com>
2023-10-27 19:48:04 +0900

    231027_벡터 구현

--- DataStruct/DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct/DataStruct.cpp
@@ -1,26 +1,12 @@
 ﻿#include <iostream>
-#include "MCqueue.h"
-
+#include "MVector.h"
 using namespace std;
 
 int main()
 {
-#pragma region 원형 큐
-	/*물리적으로는 선형 구조를 가지고 있으며, 큐의 시작점과 끝점을 연결한 큐*/
-
-	MCqueue<int> cq;
-	cq.EnQueue(10);
-	cq.EnQueue(20);
-	cq.EnQueue(30);
-
-	cq.Dequeue();
-
-	cq.EnQueue(40);
-	cq.Dequeue();
-	cout << cq.Front() << endl;
-	cout << cq.Back() << endl;
-	cout << cq.Size() << endl;
-	cq.Display();
-#pragma endregion
+	MVector<int> mv;
+	mv.Pushback(10);
+	mv.Popback();
+	mv.Popback();
 
 }
---
+++ DataStruct/DataStruct/MVector.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+#define SIZE 5
+template<typename T>
+class MVector {
+private:
+	int size;
+	int capacity;
+	T * bufferptr;
+
+public:
+	MVector() { 
+		size = 0;
+		capacity = 0;
+		bufferptr = nullptr;
+	}
+
+	void Pushback(T data) {
+		if (capacity == 0) {
+			Resize(1);
+		} else if (size >= capacity) {
+			Resize(capacity * 2);
+		}
+		
+		bufferptr[size++] = data;
+	}
+
+	void Popback() {
+		if (size-- == 0) {
+			cout << "Vector is Empty\n";
+		} else {
+			bufferptr[size--] = NULL;
+		}
+	}
+
+	void Resize(int newSize) {
+		//1.capacity에 새로운 size값을 설정.
+		capacity = newSize;
+		T * buffer = new T[capacity];
+
+		for (int i = 0; i < capacity; i++) {
+			buffer[i] = NULL;
+		}
+		for (int i = 0; i < size; i++)
+		{
+			buffer[i] = bufferptr[i];
+		}
+		if (bufferptr != nullptr) {
+			delete[] bufferptr; //배열 크기만큼 메모리 해제
+		}
+		bufferptr = buffer;
+	}
+
+	T & operator[](const int value) {
+		return bufferptr[value];
+	}
+
+	int & Size() {
+		return size;
+	}
+	int & Capacity() {
+		return capacity;
+	}
+	
+	~MVector(){ }
+};
