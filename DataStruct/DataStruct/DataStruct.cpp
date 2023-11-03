39eb128688a2425beb915995c38d05e929c6d4e0
조광래 <whrhkdfo12@naver.com>
2023-11-03 19:48:28 +0900

    231103_해시테이블

--- DataStruct/DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct/DataStruct.cpp
@@ -1,13 +1,7 @@
 ﻿#include <iostream>
-#include "MaxHeap.h"
 using namespace std;
 
 int main()
 {
-	MaxHeap<int> heap;
-	heap.Insert(30);
-	heap.Insert(20);
-	heap.Insert(50);
 
-	heap.Display();
 }
--- DataStruct/DataStruct/MaxHeap.h
+++ DataStruct/DataStruct/MaxHeap.h
@@ -8,11 +8,27 @@ private:
 	int index;
 	T arr[SIZE];
 	void SwapHeap(int child, int parent) {
-
 		if (arr[child] > arr[parent]) {
 			swap(arr[parent], arr[child]);
-			if(parent>1)
+
+			if(parent > 1)
 				SwapHeap(parent, parent/2);
+		}
+	}
+	void CheckChild(int parent) {
+		if (parent <= index) {
+			int child = parent * 2;
+
+			if (arr[child] < arr[child + 1]) {
+				child++;
+			}
+			ChangeUD(parent, child);
+			CheckChild(child);
+		}
+	}
+	void ChangeUD(int parent, int child) {
+		if (arr[parent] < arr[child]) {
+			swap(arr[parent], arr[child]);
 		}
 	}
 public:
@@ -29,9 +45,9 @@ public:
 			return;
 		} else {
 			arr[++index] = data;
-			if (index > 1) {
+			//0번지는 계산 하면 안되기에 2번지 이상이 존재할 때 swap 조건을 확인...
+			if (index > 1)
 				SwapHeap(index, (index / 2));
-			}
 			//int child = index;
 			//int parent = index / 2;
 			//
@@ -45,17 +61,63 @@ public:
 			//}
 		}
 	}
-	
+	T & DeleteRe() {
+		if (index <= 0) {
+			cout << "Heap is Empty\n";
+			exit(1);
+		}
+		//임시 변수 <- 가장 큰 값을 보관하는 역할
+		T result = arr[1];
+		//최솟값을 root에 넣어준다.
+		arr[1] = arr[index];
+		//index로 가리키는 값을 초기화
+		arr[index] = NULL;
+		index--;
+		int parent = 1;
+		while (parent * 2 <= index) {
+			int child = parent * 2;
+			//오른쪽이 클 때
+			if (arr[child] < arr[child + 1]) {
+				child++;
+			}
+			//부모 노드의 Key 값이 자식 노드의 Key 값보다 크면 반복을 종료
+			if (arr[child] < arr[parent])
+				break;
+			
+			swap(arr[parent], arr[child]);
+			parent = child;
+		}
+		return result;
+	}
+	void Delete() {
+		if (IsEmpty()) {
+			cout << "MaxHeap is Empty\n";
+			return;
+		} else {
+			T temp = arr[1];
+			arr[1] = arr[index];
+			arr[index--] = NULL;
+			
+			//index 크기를 1개 줄인 뒤, root와 자식들을 비교...
+			CheckChild(1);
+		}
+	}
 	bool IsFull() {
 		cout << boolalpha;
 		return (index == SIZE - 1) ? true : false;
 	}
-
+	bool IsEmpty() {
+		cout << boolalpha;
+		return (index == 0) ? true : false;
+	}
 	void Display() {
 		for (int i = 1; i < SIZE; i++)
 		{
 			cout << arr[i] << " ";
 		}
+		//for (const T & element : arr) {
+		//	cout << element << " "
+		//}
 		cout << endl;
 	}
 };
