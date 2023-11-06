de7eb967f00ca86af3fe467d776523aab89a92ee
조광래 <whrhkdfo12@naver.com>
2023-11-06 19:49:43 +0900

    231106 해시_테이블_2

--- DataStruct/DataStruct/DataStruct.cpp
+++ DataStruct/DataStruct/DataStruct.cpp
@@ -1,7 +1,14 @@
 ﻿#include <iostream>
+#include "HashTable.h"
+
 using namespace std;
 
 int main()
 {
+	HashTable<string, int> ht;
 
+	ht.Insert("50", 10);
+	ht.Insert("50", 20);
+	cout << ht.Count("50") << endl;
+	ht.Display("50");
 }
--- DataStruct/DataStruct/HashTable.h
+++ DataStruct/DataStruct/HashTable.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 #pragma region 해시 테이블
 	/*
@@ -19,8 +19,70 @@ using namespace std;
 		이중 해싱: 해시 값을 한번 더 해시 함수에 넣어 다른 함수를 도출하는 방식
 	*/
 #pragma endregion
+
+#define SIZE 5
+
+template<typename KEY, typename VALUE>
 class HashTable {
 private:
+	struct Node {
+		KEY key;
+		VALUE value;
 
+		Node * next;
+	};
+	struct Bucket {
+		int count;
+		Node * head;
+	};
+	int HashFunction(KEY key) {
+		int hashIndex = 0;
+		for (char element : key) {
+			hashIndex += element;
+		}
+		return hashIndex %= SIZE;
+	}
+	Node * CreateNode(KEY key, VALUE value) {
+		Node * newNode = new Node();
+
+		newNode->key = key;
+		newNode->value = value;
+		newNode->next = nullptr;
+
+		return newNode;
+	}
+	Bucket bucket[SIZE];
 public:
+	HashTable() {
+		for (int i = 0; i < SIZE; i++)
+		{
+			bucket[i].count = 0;
+			bucket[i].head = nullptr;
+		}
+	}
+	void Insert(KEY key, VALUE value) {
+		int index = HashFunction(key);
+		//새로운 노드 생성
+		Node * newNode = CreateNode(key, value);
+
+		if (bucket[index].head == nullptr) {
+			bucket[index].head = newNode;
+			bucket[index].count++;
+		} else {
+			newNode->next = bucket[index].head;
+			bucket[index].head = newNode;
+			bucket[index].count++;
+		}
+	}
+	int & Count(KEY key) {
+		return bucket[HashFunction(key)].count;
+	}
+	void Display(KEY key) {
+		int index = HashFunction(key);
+
+		while (bucket[index].head != nullptr) {
+			cout << bucket[index].head->value << " ";
+			bucket[index].head = bucket[index].head->next;
+		}
+	}
 };
